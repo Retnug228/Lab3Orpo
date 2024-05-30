648a99b196eec9ddae97df3a84992d83f4aae2eb
Retnug228 <140345168+Retnug228@users.noreply.github.com>
2024-05-30 23:22:39 +0300

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,5 +1,16 @@
 import gdown
+import zipfile
+import os
 
 url = 'https://drive.google.com/drive/folders/1RuT6VtnozMdJYvghkaohngeMl74q0pyZ?usp=drive_link'
-output = 'vosk-model-ru-0.42'
+output = 'vosk-model-ru-0.42.zip'
+
+# Загрузка модели
 gdown.download(url, output, quiet=False)
+
+# Распаковка архива
+with zipfile.ZipFile(output, 'r') as zip_ref:
+    zip_ref.extractall('vosk-model-ru-0.42')
+
+# Удаление архива после распаковки (опционально)
+os.remove(output)
