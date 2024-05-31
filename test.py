6f001561998df65d769cf62a0b40c55bfd6b58ab
Retnug228 <140345168+Retnug228@users.noreply.github.com>
2024-05-31 10:26:51 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ import ui as ui
 class TestFunctionalMethods(unittest.TestCase):
 
     @patch('functional.start_recording')
+    @patch('tkinter.Tk')
+    @patch('pyaudio.PyAudio')
     def test_start_recording_positive(self, mock_start_recording):
         # Позитивный тест для start_recording
         fn.start_recording()
@@ -13,6 +15,8 @@ class TestFunctionalMethods(unittest.TestCase):
 
     @patch('functional.is_recording', return_value=True)
     @patch('functional.start_recording')
+    @patch('tkinter.Tk')
+    @patch('pyaudio.PyAudio')
     def test_start_recording_negative(self, mock_start_recording, mock_is_recording):
         # Негативный тест для start_recording
         fn.start_recording()
@@ -20,6 +24,8 @@ class TestFunctionalMethods(unittest.TestCase):
         self.assertEqual(mock_start_recording.call_count, 1)
 
     @patch('functional.stop_recording')
+    @patch('tkinter.Tk')
+    @patch('pyaudio.PyAudio')
     def test_stop_recording_positive(self, mock_stop_recording):
         # Позитивный тест для stop_recording
         fn.stop_recording()
@@ -27,18 +33,24 @@ class TestFunctionalMethods(unittest.TestCase):
 
     @patch('functional.is_recording', return_value=False)
     @patch('functional.stop_recording')
+    @patch('tkinter.Tk')
+    @patch('pyaudio.PyAudio')
     def test_stop_recording_negative(self, mock_stop_recording, mock_is_recording):
         # Негативный тест для stop_recording
         fn.stop_recording()
         mock_stop_recording.assert_called_once()
 
     @patch('functional.choose_file_for_translation')
+    @patch('tkinter.Tk')
+    @patch('pyaudio.PyAudio')
     def test_choose_file_for_translation_positive(self, mock_choose_file_for_translation):
         # Позитивный тест для choose_file_for_translation
         ui.App.choose_file_for_translation_from_audio("valid_file.wav")
         mock_choose_file_for_translation.assert_called_once_with("valid_file.wav")
 
     @patch('functional.choose_file_for_translation')
+    @patch('tkinter.Tk')
+    @patch('pyaudio.PyAudio')
     def test_choose_file_for_translation_negative(self, mock_choose_file_for_translation):
         # Негативный тест для choose_file_for_translation
         with self.assertRaises(ValueError):
